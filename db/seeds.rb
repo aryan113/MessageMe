43632a30a9eeabb60c50af11e5ab11ae5326e100
Aryan Bhardwaj <aryanbhardwaj15199@gmail.com>
2022-08-23 16:11:05 +0530

    Build Message and User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Kalp", password: "Kalp@123")
+User.create(username: "Ashish", password: "Ashish@123")
+User.create(username: "Amit", password: "Amit@123")
+
+Message.create(body: "Hey new guy", user_id: 6)
+Message.create(body: "I'm chillin lik...", user_id: 9)
