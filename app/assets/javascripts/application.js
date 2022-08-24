9c82ecb11e84d581bdfbeb2f26a0db8b2a9b4b54
Aryan Bhardwaj <aryanbhardwaj15199@gmail.com>
2022-08-24 14:49:59 +0530

    Add restrict views at controller layer and messages from UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,10 @@
 //= require_tree .
 
 
-// To active dropdown menu. 
+// To active dropdown menu.
 $(document).on('turbolinks:load', function(){
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
